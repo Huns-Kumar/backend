fbc74dcbf572153b2beb446bbd91264bc7d74b40
Huns-Kumar <hunsjeeramani345@gmail.com>
2024-07-11 14:45:52 +0500

    register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,12 +2,13 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js"
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
-import {ApiResponse} from "../utils/ApiResponse.js;"
+import {ApiResponse} from "../utils/ApiResponse.js";
 
 
 
 const registerUser = asyncHandler(async (req, res) => {
     const { username, email, fullname, password } = req.body
+    console.log("req.body", req.body)
 
     if (
         [username, email, fullname, password].some((field) => field?.trim() === "")
@@ -18,7 +19,7 @@ const registerUser = asyncHandler(async (req, res) => {
     //    const existedUser = User.findOne({email});
     //    const existedUser = User.findOne({username});
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
 
@@ -27,18 +28,19 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    console.log("req.files", req.files)
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required");
     }
 
     const avatar = await uploadOnCloudinary(avatarLocalPath);
-
-    // Me
-    //    if(coverImageLocalPath){
-    //        const coverImage = await uploadOnCloudinary(coverImageLocalPath);
-    //    }
 
     const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -17,7 +17,8 @@ try {
     })
 
     // file has been uploaded successfully
-    console.log('file is uploaded on cloudinary', response.url)
+    // console.log('file is uploaded on cloudinary', response)
+    fs.unlinkSync(localFilePath)
     return response
 } catch (error) {
     fs.unlinkSync(localFilePath) // remove the locally save file as the upload operation got failed
